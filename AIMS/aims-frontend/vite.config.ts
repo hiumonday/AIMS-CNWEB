85199321bb62e489fce5886e21ce65f237a6c612
hiumonday <hieunt04lm@gmail.com>
2025-12-26 10:56:08 +0700

    fix: fe conf

--- AIMS/aims-frontend/vite.config.ts
+++ AIMS/aims-frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +8,7 @@ export default defineConfig({
     port: 5173,
     strictPort: true, // Fail if port is already in use
   },
-})
+  preview: {
+    allowedHosts: ["aims-frontend-cxhp.onrender.com"],
+  },
+});
